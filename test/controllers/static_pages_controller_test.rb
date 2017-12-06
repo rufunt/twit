488626f2d127d0f70f01543e26e29c03746d8bdb
rufunt <jazzoo@mail.ru>
2017-12-06 19:29:48 +0300

    Add home, about and help page views and test for title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails app by rufunt"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails app by rufunt"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails app by rufunt"
   end
 
 end
